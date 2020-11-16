328bf1b228e58d0cf555a3729c85a1dcdb1cf568
Martin Galvan <md.galvan@uniandes.edu.co>
2020-11-16 00:26:08 -0500

    Parcial 2. v124.20.2 estable

---
+++ Parcial 2/Punto 2.py
@@ -0,0 +1,144 @@
+"""
+Parcial 2
+Desarollado Por: 
+    -Martin Galvan
+    -Tomas Kavangh
+
+Punto 2: Domicilios
+"""
+
+
+#Dependecias:
+from pyomo.environ import *
+from pyomo.opt import SolverFactory
+import plotly.graph_objects as go
+import numpy as np
+import sys
+import os
+from random import randint
+
+#import ad
+
+os.system("clear")
+
+#sys.exit("Stopped")
+
+Model = ConcreteModel()
+
+# FUNCIONES**************************************************************************
+def distancia(x1,x2):
+    '''
+    Retorna la distancia Euclidiana entre dos puntos
+
+    Parameters
+    ----------
+    x1 : Tupla
+        Coordenadas X1.
+    x2 : Tupla
+        Coordenadas X2.
+
+    Returns
+    -------
+    Int
+        Distancia redondeada al entero más cercano
+
+    '''
+    return round(np.linalg.norm(np.array(x1)-np.array(x2),2))
+
+
+# SETS & PARAMETERS********************************************************************
+nodos = {1:(10,50),
+         2:(30,60),
+         3:(50,60),
+         4:(30,40),
+         5:(50,40),
+         6:(70,50)}
+
+arcos = {1:[2,4],
+         2:[1,4,3],
+         3:[2,5,6],
+         4:[1,2,5],
+         5:[3,4,6],
+         6:[3,5]
+         }
+nodosIniciales={1:1,2:4}
+nodosFinales=[3,4,5,6]
+
+
+N = RangeSet(1,len(nodos))  #Rango de Nodos
+D=RangeSet(3,6)             #Nodos finales
+R=RangeSet(1,2)             #Repartidores
+
+'''
+Calculo de costos
+'''
+costos = {i:{} for i in N}
+for nodo in N:
+    vecinos = arcos[nodo]
+    for nodo2 in N:
+        if nodo2 in vecinos:
+            costos[nodo][nodo2]=distancia(nodos[nodo],nodos[nodo2]) + randint(0,5)
+        else:
+            costos[nodo][nodo2]=999
+            
+# VARIABLE*****************************************************************************
+Model.x=Var(R,N,N,domain=Binary)
+
+# OBJETIVO*****************************************************************************
+Model.func = Objective(expr = sum(Model.x[r,i,j]*costos[i][j] for j in N for i in N for r in R))
+
+# RESTRICCIONES************************************************************************
+def unPedido(Model,r):
+    '''
+    Todos los repartidores tienen que tener almenos un pedido
+    '''
+    return sum(Model.x[r,i,j] for j in N for i in N)>=1
+Model.res1 = Constraint(R,rule=unPedido)
+
+def unSentido(Model,r,i,j):
+    '''
+    Solo se puede atravezar el camino en sentido i->j o j->i
+    '''
+    return Model.x[r,i,j]+Model.x[r,j,i]<=1
+Model.res2 = Constraint(R,N,N, rule = unSentido)
+
+def unRepartidorCamino(Model,i,j):
+    '''
+    Un repartidor solo puede pasar por ese camino
+    '''
+    return sum(Model.x[r,i,j] for r in R)<=1
+Model.res3 = Constraint(N,N,rule=unRepartidorCamino)
+
+def origen(Model,r,i):
+    '''
+    Un nodo origen solo puede ir a 1 nodo no origen
+    Si el nodo origen no pertenece a ese repartidor, no puede salir a ningun lado de ese nodo
+    '''
+    lista = list(nodosIniciales.values())
+    if i in lista:
+        if i==nodosIniciales[r]:
+            return sum(Model.x[r,i,j] for j in N if j not in lista)==1
+        else:
+            return sum(Model.x[r,i,j] for j in N)==0
+    else:
+        return Constraint.Skip
+Model.res4 =Constraint(R,N,rule=origen)
+
+def unRepartidorEntra(Model,j):
+    '''
+    Solo el repartidor 1 o el 2 puede entrar al nodo J por cualquier camino
+    '''
+    lista = list(nodosIniciales.values())
+    if j not in lista:
+        return sum(Model.x[1,i,j] for i in N) + sum(Model.x[2,i,j] for i in N)==1
+    else:
+        return Constraint.Skip
+Model.res5 = Constraint(N,rule=unRepartidorEntra)
+    
+
+
+SolverFactory('glpk').solve(Model)
+Model.display()
+
+
+print("########################################################################################")
